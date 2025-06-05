da062d056974702adafbe0f39eb8b8931826cda0
JohnQuimson <john.quimson777@gmail.com>
2025-06-05 14:31:40 +0200

    isntall: Shadcn ui

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
-         <body className={`${inter.className}`}>{children}</body>
+         <body className={`${inter.className}`}>
+            {/* header */}
+            <main className="min-h-screen">{children}</main>
+            {/* footer */}
+            <footer>
+               <div className="container mx-auto px-4 py-8">
+                  <p>footer</p>
+               </div>
+            </footer>
+         </body>
       </html>
    );
 }
