5f8b09d5da5fe2b72f7b695d2531480756d9f24f
JohnQuimson <john.quimson777@gmail.com>
2025-06-05 14:36:28 +0200

    feat: add Button component and update Home page to use it

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-   return <div>Ciao</div>;
+   return <Button>clicca</Button>;
 }
